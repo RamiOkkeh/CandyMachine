ea30a4bfc062249085516621225a31381e490538
RamiOkkeh <ramiokkeh@gmail.com>
2021-04-22 14:21:29 +0300

    created candy grid

--- src/components/machine/Candy/index.jsx
+++ src/components/machine/Candy/index.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import "./style.css"
 
 class Candy extends React.Component {
     constructor(props){
--- src/components/machine/index.jsx
+++ src/components/machine/index.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Candy from "./Candy"
+import "./style.css"
 
 class Machine extends React.Component {
     constructor(props){
@@ -8,13 +9,19 @@ class Machine extends React.Component {
             currentMoney: 0,
             earned: 0,
             snacks: [["soda", 10, 2.2], ["chips", 5, 3.5], ["chokolate", 1, 10.5], ["candy4", 12, 3], ["candy5", 5, 2],
-                     ["candy6", 10, 1]],
+                     ["candy6", 10, 1], ["chips", 5, 3.5], ["chokolate", 1, 10.5], ["candy4", 12, 3], ["candy5", 5, 2],
+                     ["candy6", 10, 1], ["chips", 5, 3.5], ["chokolate", 1, 10.5], ["candy4", 12, 3], ["candy5", 5, 2],
+                     ["candy6", 10, 1], ["chips", 5, 3.5], ["chokolate", 1, 10.5], ["candy4", 12, 3], ["candy5", 5, 2],
+                     ["candy6", 10, 1], ["chips", 5, 3.5], ["chokolate", 1, 10.5], ["candy4", 12, 3], ["candy5", 5, 2],],
         }
     }
 
     render(){
         return(<div className="machine">
+
+            <div className="candies">
                 {this.state.snacks.map((info, i)=><Candy key={i} num={i} info={info}></Candy>)}
+            </div>
         </div>)
     }
 }
