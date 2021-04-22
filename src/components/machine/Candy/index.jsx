cf8f777be45ad0e6f11f51eff7633f0a805578db
RamiOkkeh <ramiokkeh@gmail.com>
2021-04-22 20:46:21 +0300

    remove stock display

--- src/components/machine/Candy/index.jsx
+++ src/components/machine/Candy/index.jsx
@@ -5,7 +5,7 @@ class Candy extends React.Component {
     render(){
         let {num, info, image} = this.props, [name, available, price] = info
         return(<div className="candy" style={{backgroundImage: `url(${image})`}}>
-                <div>{name} {available}
+                <div>{name}
                 <span className="price"> {price}$ </span> 
                 <span className="num">{num}</span> </div>
         </div>)
